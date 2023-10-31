15f4a1bc697f726d9084da5be80e984b8bb6c73e
younex01 <younesyounex1@gmail.com>
2023-10-31 17:52:31 -0400

    refactor:mariadb and wordpress

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,10 +3,14 @@
 service mariadb start
 
 sleep 5
+if [ ! -d "/var/lib/mysql/$MYSQL_DB" ]
+then
+    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
+    mysql -u root -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" 
+    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;" && mysql -u root -e "FLUSH PRIVILEGES;"
+    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+fi
 
-mysql -u root -e "create database $MYSQL_DB;"
-mysql -u root -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" 
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;" &&mysql -e "FLUSH PRIVILEGES;"
-mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+kill $(cat /var/run/mysqld/mysqld.pid)
 
 mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-if [! -f "/var/www/html/wp-config.php"]
+if [ ! -f "/var/www/html/wp-config.php" ];
 then
-    wp core download
-    chown -R root:root /var/www/html/wordpress
-    wp core config --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD
-    wp core install --url=$DOMAIN_NAME --title="yelousse blog" --admin_user="odyn" --admin_password="valhalla" --admin_email="odyn@gmail.com"
-    wp user create yelousse yelousse@gmail.com --user_pass="Yelousse123"
+    wp core download --allow-root
+    wp core config --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
+    wp core install --url=$DOMAIN_NAME --title="yelousse blog" --admin_user="odyn" --admin_password="valhalla" --admin_email="odyn@gmail.com" --allow-root
+    wp user create yelousse yelousse@gmail.com --user_pass="Yelousse123" --allow-root
 fi
