030020cbf9d9116917cee79a9f74af9e5a723838
younex01 <younesyounex1@gmail.com>
2023-11-03 20:34:06 -0400

    refactor:all project

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -3,14 +3,13 @@
 service mariadb start
 
 sleep 5
-if [ ! -d "/var/lib/mysql/$MYSQL_DB" ]
-then
-    mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
-    mysql -u root -e "CREATE USER '$MYSQL_USER'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';" 
-    mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'localhost' WITH GRANT OPTION;" && mysql -u root -e "FLUSH PRIVILEGES;"
-    mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
-fi
+
+mysql -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
+mysql -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" 
+mysql -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
+mysql -u root -e "FLUSH PRIVILEGES;"
+
+sed -i  "s/127.0.0.1/0.0.0.0/g"  /etc/mysql/mariadb.conf.d/50-server.cnf
 
 kill $(cat /var/run/mysqld/mysqld.pid)
-
-mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
+
 if [ ! -f "/var/www/html/wp-config.php" ];
 then
     wp core download --allow-root
-    wp core config --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --allow-root
+    wp core config --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
     wp core install --url=$DOMAIN_NAME --title="yelousse blog" --admin_user="odyn" --admin_password="valhalla" --admin_email="odyn@gmail.com" --allow-root
     wp user create yelousse yelousse@gmail.com --user_pass="Yelousse123" --allow-root
 fi
