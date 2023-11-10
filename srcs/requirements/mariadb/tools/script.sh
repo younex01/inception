f5aef04a78ddf2d42d0312853ce48adb60dfb9b9
younex01 <younesyounex1@gmail.com>
2023-11-10 23:52:55 +0100

    fix: env variable for mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -2,13 +2,11 @@
 
 service mariadb start
 sleep 2
-mysql -h localhost -u root -e "CREATE DATABASE IF NOT EXISTS $MYSQL_DB;"
-mysql -h localhost -u root -e "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';" 
-mysql -h localhost -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
-mysql -h localhost -u root -e "FLUSH PRIVILEGES;"
-mysql -h localhost -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
-
 sed -i 's@= 127.0.0.1@= 0.0.0.0@' /etc/mysql/mariadb.conf.d/50-server.cnf
+mysql -u root -e "create database $MARIA_DB;"
+mysql -u root -e "create user '$MARIA_USER'@'%' identified by '$MARIA_PASSWORD';"
+mysql -u root -e "grant all privileges on $MARIA_DB.* to '$MARIA_USER'@'%';"
+mysql -u root -e "alter user 'root'@'localhost' identified by '$MARIA_PASSWORD';"
 kill $(cat /var/run/mysqld/mysqld.pid)
 
-mysqld_safe
+mysqld
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 sed -i 's/listen = \/run\/php\/php8.2-fpm.sock/listen = 9000/g' /etc/php/8.2/fpm/pool.d/www.conf
-chown -R www-data:www-data /var/www/html/
+# chown -R www-data:www-data /var/www/html/
 
 if [ ! -f "/var/www/html/wp-config.php" ];
 then
     wp core download --allow-root
-    wp core config --dbname=$MYSQL_DB --dbuser=$MYSQL_USER --dbpass=$MYSQL_PASSWORD --dbhost=$MYSQL_HOST --allow-root
+    wp core config --dbname=$MARIA_DB --dbuser=$MARIA_USER --dbpass=$MARIA_PASSWORD --dbhost=$MARIA_HOST --allow-root
     wp core install --url=$DOMAIN_NAME --title="yelousse blog" --admin_user=$WP_ROOT --admin_password=$WP_ROOT_PASSWORD --admin_email=$WP_ROOT_EMAIL --allow-root
     wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_PASSWORD --allow-root
 fi
